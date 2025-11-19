f6de7f444582657e53d6b8633c4c9144c2f7b56d
Max <z.max83@mail.ru>
2025-11-19 23:08:59 +0300

    коммит16

--- config/settings.py
+++ config/settings.py
@@ -112,7 +112,10 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATIC_ROOT = '/opt/diploma_personal_diary/static/'
+
+STATIC_ROOT = [
+    BASE_DIR / "static"
+]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
